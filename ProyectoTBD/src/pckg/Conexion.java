5ad84ca52bbc38ede0076585eba0c98f77ab6200
Jonathan Gonzalez <125224673+jony-glz@users.noreply.github.com>
2025-05-30 20:32:12 -0600

    Update Conexion.java

--- ProyectoTBD/src/pckg/Conexion.java
+++ ProyectoTBD/src/pckg/Conexion.java
@@ -6,10 +6,10 @@ public class Conexion {
     
     public boolean conectar(){
         String wallet = System.getProperty("user.dir")+"\\src\\pckg\\Wallet_Proyecto";
-        String url = "jdbc:oracle:thin@(description=(address_list=(address=(protocol=tcps)(host=adb.us-ashburn-1.oraclecloud.com)(port=1522)))(connect_data=(service=gb56437d6ad9cd9_proyecto_high.adb.oraclecloud.com)))";
+        String url = "jdbc:oracle:thin:@proyecto_high";
         
         try{
-            System.setProperty("oracle.net.wallet_location", wallet);
+            System.setProperty("oracle.net.tns_admin", wallet);
             conexion = DriverManager.getConnection(url, "ADMIN", "GonzalezRamos596365");
             
             if (conexion != null && !conexion.isClosed()) {
